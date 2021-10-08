f5909a1d832512ecb4d9da272da5293cc344e56e
SalmonDE <salmonde@salmonde.de>
2021-10-08 20:48:36 +0200

    UpdateNotifyTask: make use of InternetRequestResult

--- src/BlockHorizons/BlockSniper/task/UpdateNotifyTask.php
+++ src/BlockHorizons/BlockSniper/task/UpdateNotifyTask.php
@@ -17,12 +17,12 @@ class UpdateNotifyTask extends AsyncTask{
 	private const RELEASES_URL = "https://poggit.pmmp.io/releases.json?name=BlockSniper";
 
 	public function onRun() : void{
-		$json = Internet::getURL(self::RELEASES_URL, 10, [], $err);
+		$requestResult = Internet::getURL(self::RELEASES_URL, 10, [], $err);
 		$highestVersion = Loader::VERSION;
 		$artifactUrl = "";
 		$api = "";
-		if($json !== false){
-			$releases = json_decode($json, true);
+		if($requestResult->getCode() === 200){
+			$releases = json_decode($requestResult->getBody(), true);
 			foreach($releases as $release){
 				if(version_compare($highestVersion, $release["version"], ">=")){
 					continue;
